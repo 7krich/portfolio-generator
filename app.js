b0d0dce1989e2cc894233bb4360c86daeeef6cce
Kyle Richnafsky <7krich@gmail.com>
2022-04-03 11:59:36 -0400

    updated questions now write to HTML

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -137,11 +139,10 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-    // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+
+    const pageHTML = generatePage(portfolioData);
+    fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+    console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
